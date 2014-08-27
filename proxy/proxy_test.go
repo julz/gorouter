d7a2689973e99e7798a095de176c7c2fe85dd51e
Julian Friedman <julz.friedman@uk.ibm.com>
2014-08-27 16:23:24 +0100

    WIP - Do not encode reserved characters
    There are a set of reserved characters in rfc 3986 which are not in
    2396, unfortunately golang does not include these characters in url.go
    and therefore they get percent-encoded in the proxy rather than being
    passed through (note this is because gorouter is not currently a
    fully transparently proxy -- it decodes and re-encodes the URLs on the way
    through). This commit duplicates the parsing logic from golang's version
    of url.go in order to add the extra reserved characters from rfc 3986.
    
    [#77014252]

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -774,20 +774,16 @@ var _ = Describe("Proxy", func() {
 	})
 
 	It("maintains most percent-encoded values in URLs", func() {
-		shouldEcho("/abc%25%20%2A%21%22%3F%5Edef", "/abc%25%20%2A%21%22%3F%5Edef") // %, <space>, *, !, ", £, ^
+		shouldEcho("/abc%25%20%22%3F%5Edef", "/abc%25%20%22%3F%5Edef") // %, <space>, ", £, ^
 	})
 
 	It("decodes some percent encoded characters in URLs", func() {
 		shouldEcho("/abc%2b%2fdef", "/abc+/def")
 	})
 
-	It("encodes reserved characters in URLs", func() {
-		shouldEcho("/abc!*'()def", "/abc%21%2A%27%28%29def")
-	})
-
-	It("fails to encode certain reserved characters in URLs", func() {
+	It("does not encode reserved characters in URLs", func() {
 		rfc3986_reserved_characters := "!*'();:@&=+$,/?#[]"
-		shouldEcho("/abc"+rfc3986_reserved_characters+"def", "/abc%21%2A%27%28%29;:@&=+$,/?#[]def")
+		shouldEcho("/"+rfc3986_reserved_characters, "/"+rfc3986_reserved_characters)
 	})
 
 	It("does not encode unreserved characters in URLs", func() {
@@ -795,7 +791,7 @@ var _ = Describe("Proxy", func() {
 	})
 
 	It("percent-encodes special characters in URLs", func() {
-		shouldEcho("/abc*!\"£^def", "/abc%2A%21%22%C2%A3%5Edef")
+		shouldEcho("/abc\"£^def", "/abc%22%C2%A3%5Edef")
 	})
 
 	It("handles requests with encoded query strings", func() {
---
+++ proxy/urlescape.go
@@ -0,0 +1,98 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Package url parses URLs and implements query escaping.
+// See RFC 3986.
+
+// this file reproduces the parsing logic from url.go solely
+// in order to modify the list of reserved characters to include
+// the extra characters from RFC 3986 (!, *, *, ', (, )).
+
+package proxy
+
+type encoding int
+
+const (
+	encodePath encoding = 1 + iota
+	encodeUserPassword
+	encodeQueryComponent
+	encodeFragment
+)
+
+func shouldEscape(c byte, mode encoding) bool {
+	// §2.3 Unreserved characters (alphanum)
+	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
+		return false
+	}
+
+	switch c {
+	case '-', '_', '.', '~': // §2.3 Unreserved characters (mark)
+		return false
+
+	case '$', '&', '+', ',', '/', ':', ';', '=', '?', '@', '!', '*', '\'', '(', ')': // §2.2 Reserved characters (reserved)
+		// Different sections of the URL allow a few of
+		// the reserved characters to appear unescaped.
+		switch mode {
+		case encodePath: // §3.3
+			// The RFC allows : @ & = + $ but saves / ; , for assigning
+			// meaning to individual path segments. This package
+			// only manipulates the path as a whole, so we allow those
+			// last two as well. That leaves only ? to escape.
+			return c == '?'
+
+		case encodeUserPassword: // §3.2.2
+			// The RFC allows ; : & = + $ , in userinfo, so we must escape only @ and /.
+			// The parsing of userinfo treats : as special so we must escape that too.
+			return c == '@' || c == '/' || c == ':'
+
+		case encodeQueryComponent: // §3.4
+			// The RFC reserves (so we must escape) everything.
+			return true
+
+		case encodeFragment: // §4.1
+			// The RFC text is silent but the grammar allows
+			// everything, so escape nothing.
+			return false
+		}
+	}
+
+	// Everything else must be escaped.
+	return true
+}
+
+func escape(s string, mode encoding) string {
+	spaceCount, hexCount := 0, 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if shouldEscape(c, mode) {
+			if c == ' ' && mode == encodeQueryComponent {
+				spaceCount++
+			} else {
+				hexCount++
+			}
+		}
+	}
+
+	if spaceCount == 0 && hexCount == 0 {
+		return s
+	}
+
+	t := make([]byte, len(s)+2*hexCount)
+	j := 0
+	for i := 0; i < len(s); i++ {
+		switch c := s[i]; {
+		case c == ' ' && mode == encodeQueryComponent:
+			t[j] = '+'
+			j++
+		case shouldEscape(c, mode):
+			t[j] = '%'
+			t[j+1] = "0123456789ABCDEF"[c>>4]
+			t[j+2] = "0123456789ABCDEF"[c&15]
+			j += 3
+		default:
+			t[j] = s[i]
+			j++
+		}
+	}
+	return string(t)
+}
